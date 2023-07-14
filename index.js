ca5d0b8405716ad3e404ffa34d60c3906b9affc6
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-07-14 00:00:42 -0400

    complete control lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(customer){
   // Write your code here!
+  if(customer < 400) {
+    return 'This one is on me!'
+  }else if(customer >400 && customer <2000){
+    return 'That will be twenty bucks.'
+  }else if(customer>2000 && customer <2500){
+    return 'I will gladly take your thirty bucks.'
+  }else if (customer > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city === 'NYC'){
+    return 'Ok, sounds good.'
+  }else{
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
+  if(generous === 'generous'){
+    return 'Thank you so much.'
+  }else if(generous === 'not as generous'){
+    return 'Thank you.'
+  }else{
+    return 'Bye.'
+  }
+
+
 }
